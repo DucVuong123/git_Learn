94a619e59f046fc2b150f44bc5aa5ca727407beb
DESKTOP-RHVHV7A\Admin <doducvuong123v@gmail.com>
2025-01-13 22:15:23 +0700

    version5

--- conflig/Program.cs
+++ conflig/Program.cs
@@ -11,7 +11,7 @@ namespace conflig
         static void Main(string[] args)
         {
             string s="nsssll";
-            string c = "sss";
+            string c = "version5";
         }
     }
 }
